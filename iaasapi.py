cbdf19489f14131c7c4aea5f8a0a584e83005eb2
turtleender <tpiperatgod@gmail.com>
2013-08-02 11:06:43 +0800

    none

--- iaasapi.py
+++ iaasapi.py
@@ -4,32 +4,66 @@
 
 from __future__ import with_statement
 from flask import Flask, request, session, g, redirect, url_for, abort, \
-     render_template, flash
+    render_template, flash
 from hashlib import md5
 import time
-import sys 
+import sys
 import types
-reload(sys) 
-sys.setdefaultencoding('utf8') 
-import logging
+reload(sys)
+sys.setdefaultencoding('utf8')
 from contextlib import closing
 
 DEBUG = True
 SECRET_KEY = 'I AM TURTLE ENDER'
 
 app = Flask(__name__)
-app.config.from_object('iaasapi.config')
+app.config.from_object('config')
 
-@app.before_request     
+
+@app.before_request
 def before_request():
     pass
-        
+
+
 def teardown_request(exception):
     pass
 
+
+def get_token(host_ip):
+    global G_INFO
+    params = '{"auth": {"tenantName": "%s", "passwordCredentials": {"username": "%s", "password": "%s"}}}' 
+                % (G_INFO['tenant'], G_INFO['user'], G_INFO['password'])
+    headers = {"Content-type": "application/json"}
+    req = {"flag": "getToken",
+           "url": _host_ip + ':35357',
+           "method": "POST",
+           "path": SERVICE_PATH_TEMP['getToken'],
+           "params": params,
+           "headers": headers}
+    rreq = json.dumps(req)
+    rsp = json.loads(iaas_func(rreq))
+    token_id = rsp['data']['access']['token']['id']
+    try:
+        for i in range(len(rsp['data']['access']['serviceCatalog'])):
+            server_ctl = rsp['data']['access']['serviceCatalog'][i]
+            if server_ctl.get('name', None) == 'Compute Service':
+                nova_url = urlparse.urlparse(
+                    server_ctl['endpoints'][0].get('publicURL', None))
+            elif server_ctl.get('name', None) == 'Identity Service':
+                keystone_adminurl = urlparse.urlparse(
+                    server_ctl['endpoints'][0].get('adminURL', None))
+                keystone_publicurl = urlparse.urlparse(
+                    server_ctl['endpoints'][0].get('publicURL', None))
+    except e:
+        LOG.error("Can not get server catalog, error: %(error)s", {'error': e})
+
+
+    return token_id, nova_url, keystone_adminurl, keystone_publicurl
+
+
 @app.route('/')
 def index():
-    print app.config.RABBITMQ_IP
+    print app.config
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='8080')
+    app.run(host='0.0.0.0', port=8080)
---
+++ logs/log.py
@@ -0,0 +1,17 @@
+import logging
+import logging.config
+
+logging.config.fileConfig("logging.conf")
+
+#create logger
+logger = logging.getLogger("example")
+
+#"application" code
+logger.debug("debug message")
+logger.info("info message")
+logger.warn("warn message")
+logger.error("error message")
+logger.critical("critical message")
+
+logHello = logging.getLogger("hello")
+logHello.info("Hello world!")
